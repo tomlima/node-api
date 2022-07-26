4391aae934faa05baef1df4469991660da7159f0
Tom lima <dev@tomlima.me>
2022-07-26 14:37:49 -0300

    Add support to .env file

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const {MongoClient} = require("mongodb")
+require('dotenv').config()
+
 
 const database = {
   connection: async () => {
-    const conn = await MongoClient.connect(`mongodb://tom:tom@localhost:27017/apiDB`)
-    const client = conn.db("apiDB")
+    const conn = await MongoClient.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
+    const client = conn.db(process.env.DB_NAME)
     if(!conn) return new Error("Cant connect")
     return client
   }
